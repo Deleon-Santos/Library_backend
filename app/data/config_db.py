cb5c276b34cc0d3f160b41ff3e987c55e54a5456
Deleon <delps.santos1987@gmail.com>
2026-03-14 21:53:15 -0300

    feat: adiciona o metodo de conexao com banco de dados online

--- app/data/config_db.py
+++ app/data/config_db.py
@@ -1,11 +1,27 @@
 
+import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import app
+# from flask import app
+from dotenv import load_dotenv
 
+load_dotenv()
 
-engine = create_engine("sqlite:///db.library" , echo= True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://", 1):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,
+        pool_size=5,
+        max_overflow=10
+    )
+else:
+
+    engine = create_engine("sqlite:///db.library" , echo= True)
 Session = sessionmaker(bind=engine) 
 Base = declarative_base()
 
